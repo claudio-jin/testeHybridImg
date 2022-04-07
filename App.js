19ec4ef4e8c4231603b1d4826cff9dfe3618d8c9
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-07 11:59:58 -0300

    uso de imagens react native

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { 
+  View,
+  StatusBar,
+  Image,
+  Button
+ } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//  import img from ''
+
+export default class App extends React.Component {
+
+  constructor(props)  {
+    super(props)
+    this.state = {
+      resizeMode : 'cover'
+    }
+  }
+
+  render () {
+    return(
+      <View style = {{marginTop: 20}}>
+        <Image
+        resizeMode = {this.state.resizeMode}
+          source = {{uri : 'https://picsum.photos/id/10/300/200.jpg'}}
+          style = {{width: 300, height: 200, alignSelf : 'center'}}
+        />
+
+        <View>
+          <Button 
+          title = "Cover" 
+          onPress={_=> this.setState({resizeMode : "cover"})}
+          />
+          <Button 
+          title = "Contain" 
+          onPress={_=> this.setState({resizeMode : "contain"})}
+          />
+          <Button 
+          title = "Stretch" 
+          onPress={_=> this.setState({resizeMode : "stretch"})}
+          />
+          <Button 
+          title = "Repeat" 
+          onPress={_=> this.setState({resizeMode : "repeat"})}
+          />
+          <Button 
+          title = "Center" 
+          onPress={_=> this.setState({resizeMode : "center"})}
+          />
+        </View>
+      <StatusBar/>
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
